b38a75b55daa4b49102d7a014250112f60724105
Michael Ramos <ramosmichael@live.com>
2022-02-24 04:57:04 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form =document.querySelector('form')
+  
+  form.addEventListener('submit',(e) => {
+    
+    e.preventDefault()
+    
+    makeList(e.target.new_task_description.value)
+    
+    form.reset()
+  })
+
+
+  function makeList(input) {
+    const p = document.createElement('p')
+    
+    const btn = document.createElement('button')
+    
+    p.textContent = `${input} `
+    
+    p.appendChild(btn)
+    
+    btn.textContent = 'X'
+    
+    document.querySelector('#list').appendChild(p)
+    
+    btn.addEventListener('click', () => {
+      p.remove()
+    })
+
+
+  }
+
+
 });
